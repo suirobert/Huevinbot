09517d2d1797acaa58f0d7f7464e092e5a07724c
suirobert <102192127+suirobert@users.noreply.github.com>
2025-04-19 14:06:25 -0500

    Update config.py
    Aquí almacenaremos las configuraciones globales, como variables de entorno y constantes.

--- bot/config.py
+++ bot/config.py
@@ -1,1 +1,18 @@
+import os
+from dotenv import load_dotenv
+from datetime import timedelta
 
+load_dotenv()
+
+# Configuración de Spotify
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+
+# Configuración de OpenAI
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+# Configuraciones del bot
+ALLOWED_CHANNEL_ID = 1108528856408805417
+ALLOWED_ROLE_ID = 714948180617330728
+CONVERSATION_TIMEOUT = timedelta(minutes=30)
+MAX_HISTORY = 3
