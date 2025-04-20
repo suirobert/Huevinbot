c735b9a3761487386041bc46858ad4e6734b0c16
suirobert <102192127+suirobert@users.noreply.github.com>
2025-04-20 10:33:24 -0500

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from .music import setup_music_commands
 from .chat import setup_chat_commands
+from .freegames import setup_freegames, check_free_games
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -14,11 +15,16 @@ async def on_ready():
     activity = discord.Game(name="insultando pendejos")
     await bot.change_presence(activity=activity)
     print(f"Bot conectado como {bot.user}")
+    # Iniciar la tarea de juegos gratis
+    if not check_free_games.is_running():
+        check_free_games.start(bot)
+        print("Tarea de juegos gratis iniciada.")
 
 # Configurar los comandos
 def setup():
     setup_music_commands(bot)
     setup_chat_commands(bot)
+    setup_freegames(bot)
 
 # Ejecutar la configuración
 setup()
