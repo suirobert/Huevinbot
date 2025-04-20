d2ec8bb3ff152b1dc37f7e1a922b7f0cf3d614f2
suirobert <102192127+suirobert@users.noreply.github.com>
2025-04-19 23:31:33 -0500

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,32 +1,24 @@
 import discord
 from discord.ext import commands
-import os
-from dotenv import load_dotenv
-from bot.music import setup_music_commands
-# Importa otros módulos según sea necesario
-
-load_dotenv()
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+from .music import setup_music_commands
+from .chat import setup_chat_commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-intents.members = True
+intents.voice_states = True
 
-bot = commands.Bot(command_prefix="-", intents=intents)
+bot = commands.Bot(command_prefix='-', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Bot conectado como {bot.user}")
-    # Establecer la actividad del bot
-    activity = discord.Activity(type=discord.ActivityType.playing, name="insultando pendejos")
+    activity = discord.Game(name="insultando pendejos")
     await bot.change_presence(activity=activity)
+    print(f"Bot conectado como {bot.user}")
 
-# Configurar comandos de música
-setup_music_commands(bot)
+# Configurar los comandos
+def setup():
+    setup_music_commands(bot)
+    setup_chat_commands(bot)
 
-# Configurar otros módulos según sea necesario
-# Por ejemplo:
-# from bot.moderation import setup_moderation_commands
-# setup_moderation_commands(bot)
-
-bot.run(DISCORD_TOKEN)
+# Ejecutar la configuración
+setup()
